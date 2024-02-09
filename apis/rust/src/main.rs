5ba1ac2817a1f143009b62f6fac8d02b253f7700
saffronjam <own.emka@gmail.com>
2024-02-09 16:59:22 +0100

    Add Rust API

---
+++ apis/rust/src/main.rs
@@ -0,0 +1,28 @@
+use actix_files as fs;
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use uuid::Uuid;
+
+async fn index() -> impl Responder {
+    HttpResponse::Found().header("Location", "/static/index.html").finish()
+}
+
+async fn resource() -> impl Responder {
+    HttpResponse::Ok().json(serde_json::json!({
+        "id": Uuid::new_v4().to_string(),
+        "name": "Hello from a Rust API"
+    }))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/v1/resource", web::get().to(resource))
+            .service(fs::Files::new("/static", "static").show_files_listing())
+            .service(fs::Files::new("/favicon.ico", "static/favicon.ico"))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
+}
